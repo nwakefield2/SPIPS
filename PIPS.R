8d91f8c4c48170708a64056ea968530447db5b25
Nathan Wakefield <43244840+nwakefield2@users.noreply.github.com>
2021-06-18 11:13:22 -0500

    Update PIPS.R

--- PIPS.R
+++ PIPS.R
@@ -31,23 +31,31 @@ library("lavaan")
 
 cfa.model2 <-' engagement  =~ x1 + x2 + x5 + x11 + x12 + x17 + x18 + x19 + x22
 collaboration =~ x6 + x7 + x8 + x10 + x15 + x16 + x20
-participation   =~ x13 + x16 + x21
+participation   =~ x13 + x13.1 + x16 + x21
 thinking =~ x3 + x4 + x20'
 
 ## getting data ready
 
-##DataCSV <- #removed for blinding the project (Instead the data is read in separately)
+DataCSV <- read.csv(file.choose())
 DataCSV <- as.data.frame(sapply(DataCSV,gsub,pattern='\x89۪' ,replacement="'"))
-DFData <- DataCSV[,c(84,85,87,88)] #questions are in same order
-DFData <- DFData[,-9]
-DFData <- DFData[,-13]
+#Convert String to Numeric
+DataCSV <- as.data.frame(sapply(DataCSV,gsub,pattern='Very descriptive' ,replacement="5"))
+DataCSV <- as.data.frame(sapply(DataCSV,gsub,pattern='Mostly descriptive' ,replacement="4"))
+DataCSV <- as.data.frame(sapply(DataCSV,gsub,pattern='Somewhat descriptive' ,replacement="3"))
+DataCSV <- as.data.frame(sapply(DataCSV,gsub,pattern='Minimally descriptive' ,replacement="2"))
+DataCSV <- as.data.frame(sapply(DataCSV,gsub,pattern='Not at all descriptive' ,replacement="1"))
+DFData <- DataCSV[,c(82,83,85,88,90,93,94,95,96,97,98,99,107,108,109,110,111,117,118,119,120,121,122)] #questions are in same order
+##DFData <- DFData[,-9]
+##DFData <- DFData[,-13]
+DFData<-apply(DFData,1,as.numeric)
+DFData<-t(DFData)
 t.f.remove <- apply(DFData, 1, should.remove2) # should row be removed
 DFData <- as.data.frame(DFData[!t.f.remove, ])
 DFDataM<-as.matrix(DFData)
 DFDataM<-apply(DFDataM,1,as.numeric)
 DFDataM<-t(DFDataM)
 row.names(DFDataM) <- NULL
-colnames(DFDataM) <- c("x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x10", "x11", "x12", "x13", "x15", "x16", "x17", "x18", "x19", "x20", "x21", "x22")
+colnames(DFDataM) <- c("x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8","x9", "x10", "x11", "x12", "x13","x14","x13.1", "x15", "x16", "x18", "x17", "x19", "x20", "x21", "x22")
 
 
 ## testing data w cfa
